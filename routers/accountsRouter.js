0948cd7a35fb52062820e9783680dc4fdaafe382
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-05-04 20:28:13 -0400

    CRUD operations completed for accounts resource. MVP complete

--- routers/accountsRouter.js
+++ routers/accountsRouter.js
@@ -16,12 +16,56 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .then((acct) => {
+      res.status(200).json(acct);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "server error. Unable to retrieve account" });
+    });
+});
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  db("accounts")
+  .insert(req.body, "id")
+  .then( acct => {
+    res.status(201).json(acct)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({ message: "server error. Unable to create account" })
+  })
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  db("accounts")
+  .where({ id: req.params.id})
+  .update(req.body)
+  .then(changes => {
+    res.status(200).json(changes);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({message: "server error. Unable to update account"})
+  })
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  db("accounts")
+  .where({ id: req.params.id})
+  .del()
+  .then(deleted => {
+    res.status(204).json({message: "succesfully deleted account"})
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({message: "server error. Unable to delete account"})
+  })
+});
 
 module.exports = router;
